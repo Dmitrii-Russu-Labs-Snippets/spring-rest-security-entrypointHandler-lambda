38a4ac3adec963905ea39a8d558d6d3cbc10def2
Dmitrii Russu <dmitriirussulink@.gmail.com>
2025-09-27 11:22:51 +0300

    Initial commit: project upload

---
+++ src/main/java/com/example/demo_rest_2/security/config/SecurityConfig.java
@@ -0,0 +1,112 @@
+package com.example.demo_rest_2.security.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.net.URI;
+import java.time.Instant;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final ObjectMapper mapper;
+
+    public SecurityConfig(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Bean
+    public SecurityFilterChain web(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/welcome").permitAll()
+                        .requestMatchers("/auth/user").hasRole("USER")
+                        .requestMatchers("/auth/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                //.csrf(AbstractHttpConfigurer::disable)
+                .cors(withDefaults())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // <-- тут
+                )
+                .httpBasic(withDefaults())
+                .exceptionHandling(ex -> ex
+                        // 401 Unauthorized
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            ProblemDetail pd = ProblemDetail.forStatusAndDetail(
+                                    HttpStatus.UNAUTHORIZED, "Authentication required"
+                            );
+                            // Добавляем дополнительные поля
+                            pd.setInstance(URI.create(request.getRequestURI()));
+                            pd.setProperty("timestamp", Instant.now().toString());
+                            pd.setProperty("path", request.getRequestURI());
+
+                            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                            response.setContentType("application/problem+json");
+                            mapper.writeValue(response.getWriter(), pd);
+                        })
+                        // 403 Forbidden
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            ProblemDetail pd = ProblemDetail.forStatusAndDetail(
+                                    HttpStatus.FORBIDDEN, "Access denied"
+                            );
+                            // Добавляем дополнительные поля
+                            pd.setInstance(URI.create(request.getRequestURI()));
+                            pd.setProperty("timestamp", Instant.now().toString());
+                            pd.setProperty("path", request.getRequestURI());
+
+                            response.setStatus(HttpStatus.FORBIDDEN.value());
+                            response.setContentType("application/problem+json");
+                            mapper.writeValue(response.getWriter(), pd);
+                        })
+                );
+                /*.exceptionHandling(ex -> ex
+                        .authenticationEntryPoint((request, response, authException) -> {
+
+                            Map<String,Object> body = new LinkedHashMap<>();
+                            body.put("timestamp", Instant.now().toString());
+                            body.put("status", HttpStatus.UNAUTHORIZED.value());
+                            body.put("error", HttpStatus.UNAUTHORIZED.getReasonPhrase()); // "Forbidden"
+                            body.put("message", "Authentication failed");
+                            body.put("path", request.getRequestURI());
+
+                            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+                            mapper.writeValue(response.getOutputStream(), body);
+                        })
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+
+                            Map<String,Object> body = new LinkedHashMap<>();
+                            body.put("timestamp", Instant.now().toString());
+                            body.put("status", HttpStatus.FORBIDDEN.value());
+                            body.put("error", HttpStatus.FORBIDDEN.getReasonPhrase()); // "Forbidden"
+                            body.put("message", "Access Denied");
+                            body.put("path", request.getRequestURI());
+
+                            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+                            mapper.writeValue(response.getOutputStream(), body);
+                        })*/
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
